df3b8f9fd6b44faf459295db58cde56cb095dd29
EFastner <eric.fastner@gmail.com>
2018-09-03 21:28:29 -0500

    Cleaned up scrape results and added checks to ensure data was retrieved correctly

--- ds.scrape_movie.R
+++ ds.scrape_movie.R
@@ -1,9 +1,22 @@
 ds.scrape_movie <- function(movieID) {
   url <- paste0("https://www.imdb.com/title/", movieID)
   
-  pagecontent <- 
-    html_session(url) %>% 
-    html_nodes("[class = 'pagecontent']")
+  try_count = 0
+  pagecontent = character(0)
+  
+  #Grab the pagecontent, check to make sure the data was grabbed or try again
+  while (length(pagecontent) == 0 & try_count < 6) {
+    pagecontent <- 
+      html_session(url) %>% 
+      html_nodes("[class = 'pagecontent']")
+    
+    try_count <- try_count + 1 
+  }
+  
+  #If the function was not able to get the movie content, return nothing
+  if (length(pagecontent) == 0) {
+    return(character(0))
+  }
   
 #CREDIT_LISTS####################################################
   credit_list <- 
@@ -12,29 +25,50 @@ ds.scrape_movie <- function(movieID) {
   
   #Grab Directors
   director <- 
-    credit_list[1] %>% 
+    credit_list[[1]] %>% 
     html_nodes("a") %>% 
     html_text()
   
-  director <- director[sapply(director, function(x) !grepl("more credit", x))]
+  director <- 
+    director[sapply(director, function(x) !grepl("more credit", x))] %>%
+    paste(collapse = ", ")
   
   #Grab Writers
   writer <- 
-    credit_list[2] %>%
+    credit_list[[2]] %>%
     html_nodes("a") %>%
     html_text()
   
-  writer <- writer[sapply(writer, function(x) !grepl("more credit", x))]
+  writer <- 
+    writer[sapply(writer, function(x) !grepl("more credit", x))] %>%
+    paste(collapse = ", ")
   
 #DETAILS#####################################################
-  title_details <- 
-    pagecontent %>% 
-    html_nodes("[id = 'titleDetails']") %>%
-    html_nodes("div") %>% html_text(trim = TRUE)
+  try_count = 0
+  title_details_text <- character(0)
+  
+  #Try to grab the title details
+  while (length(title_details_text) == 0 & try_count < 6) {
+    title_details <- 
+      pagecontent %>% 
+      html_nodes("[id = 'titleDetails']") %>%
+      html_nodes("div")
+    
+    title_details_text <- title_details %>% html_text()
+    
+    try_count <- try_count + 1
+    }
+  
+  #If the function was not able to retrieve the title details, return nothing
+  if (length(title_details_text) == 0) {
+    return(character(0))
+  }
 
   #Grab Budget
-  scrape_row <- title_details[sapply(title_details, function(x) grepl("Budget", x))]
-  budget <- gsub(paste("Budget:","\n","\\(estimated\\)"," ", sep = "|"),"", scrape_row)
+  scrape_row <- title_details_text[sapply(title_details_text, function(x) grepl("Budget", x))]
+
+  budget <- 
+    gsub(paste("Budget:","\n","\\(estimated\\)"," ", sep = "|"),"", scrape_row)
   
   #If the budget doesn't exist, change it to NA
   if (length(budget) == 0) {
@@ -42,10 +76,10 @@ ds.scrape_movie <- function(movieID) {
   }
   
   #Grab Runtime
-  scrape_row <- title_details[sapply(title_details, function(x) grepl("Runtime", x))]
-  runtime <- gsub(paste("Runtime:","\n", "min", " ", "|", sep = "|"),"", scrape_row)
+  runtime <- title_details %>% html_nodes("time") %>% html_text(trim = TRUE)
+  runtime <- as.numeric(gsub(" min", "", runtime[1]))
   
-  #If the runtime did not exist, grab from header and convert
+  #If the runtime did not exist, grab from header and convert the result to minutes
   if (length(runtime) == 0) {
     
     runtime_hrs_mins <- 
@@ -66,9 +100,9 @@ ds.scrape_movie <- function(movieID) {
     runtime <- (runtime_hrs * 60) + runtime_mins
   }
   
-  
-  movie_data <- c(budget, runtime)
-  names(movie_data) <- c("budget", "runtime")
+  #Compile all data in a vector and name each column
+  movie_data <- c(director, writer, budget, runtime)
+  names(movie_data) <- c("director", "writer", "budget", "runtime")
   
   return(movie_data)
 }
--- top_rated_movies.R
+++ top_rated_movies.R
@@ -2,6 +2,7 @@ require(rvest)
 
 url <- 'https://www.imdb.com/chart/top'
 
+error_list <- c()
 full_list <- html_session(url) %>% html_nodes("tr")
 
 movie_list <- full_list %>% 
@@ -11,19 +12,37 @@ link_list <- full_list %>%
   html_nodes("[class = 'wlb_ribbon']") %>%
   xml_attr("data-tconst")
 
+#Grab title, year, and rating
 title <- movie_list %>% html_nodes("a") %>% html_text()
 year <- movie_list %>% html_nodes("span") %>% html_text() %>% substr(2,5)
 rating <- full_list %>% html_nodes("strong") %>% html_text() %>% as.numeric()
 
+#Compile data in a data frame
 top_movies <- data.frame(ID = link_list, title, year, rating)
 
 #IN DEVELOPMENT - Cycle through each top movie to compile more data
 for (i in 1:nrow(top_movies)) {
-  print(i)
+  
+  movie_data <- ds.scrape_movie(top_movies[i, "ID"]) #Call movie scraping function
+  
+  #If data was returned from the function, add it to the existing table
+  if (length(movie_data) != 0) {
+    top_movies[i, "director"] <- movie_data["director"]
+    top_movies[i, "writer"] <- movie_data["writer"]
+    top_movies[i, "budget"] <- movie_data["budget"]
+    top_movies[i, "runtime"] <- as.numeric(movie_data["runtime"])
+    
+    print(paste(i, as.character(top_movies[i, "title"]), sep = ": "))
+  } 
+  else {
+    error_list <- append(error_list, top_movies[i, "title"]) #Compile any movies that data was not returned for
+    
+    print(paste0("Error at movie ", i, ": ", top_movies[i, "title"]))
+  }
+}
 
-  movie_data <- ds.scrape_movie(top_movies[i, "ID"])
-
-  top_movies[i, "budget"] <- movie_data["budget"]
-  top_movies[i, "runtime"] <- movie_data["runtime"]
-
+#If there were any errors, print out the results
+if (!is.null(error_list)) {
+  print(paste0("There were ", length(error_list), " movies that were not retrieved. Viewing now..."))
+  View(error_list)
 }
